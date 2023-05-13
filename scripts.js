872ddad0dfc9b7ffe804e10b1850ba3535b99626
Joshua <a8gd0ddf7hdh@gmail.com>
2023-05-13 15:00:40 +0000

    epic gamer popups

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
 var main = document.getElementById('main');
+const sub_pnum = document.getElementById('submit-pnum');
+const pnum = document.getElementById('pnum');
+const verifyP = document.getElementById('verify-pnum');
+const pnum_content = document.getElementById('pnum-content');
+
+function showPnum() {
+    pnum.style.display = 'flex';
+}
+
+function hidePnum() {
+    pnum.style.display = 'none';
+}
 
 function sike() {
     setTimeout(function () {
-        main.innerHTML = "<h1 id='sorry'>Sorry this website doesn't support your browser.</h1><br><img src='sorry.png'>"
+        main.innerHTML = "<h1 id='sorry'>Sorry this website doesn't support your browser.</h1><br><img src='sorry.png'>";
     }, 5000);
 }
 
@@ -78,14 +90,52 @@ function showVideo() {
 
 function closeMoreCookiesMenu() {
     more_cookies.style.display = 'none';
-    showVideo()
+    showVideo();
 }
 
-var close_video = document.getElementById('close-video')
+sub_pnum.addEventListener('click', closePnum);
+
+var close_video = document.getElementById('close-video');
 
 close_video.addEventListener('click', hideVideo);
 
 function hideVideo() {
     video.style.display = 'none';
-    sike();
+    showPnum();
+}
+
+function closePnum() {
+    hidePnum();
+    verifyP.style.display = "flex";
+}
+
+const subCode = document.getElementById('submit-code');
+
+subCode.addEventListener('click', submitCode);
+
+function submitCode() {
+    document.getElementById('vpc').innerHTML = "<h1>Sorry, that isn't right.</h1><br><button id='resend'>Send again?</button>";
+    document.getElementById('resend').addEventListener('click', resend);
+}
+
+function resend() {
+    document.getElementById('vpc').innerHTML = `
+    <h1>Please verify your phone number.</h1>
+    <h3>GET IT RIGHT THIS TIME!</h3>
+    <br>
+    <label for="pnum">YOUR NUMBER: </label>
+    <input id="pnum" type="text">
+    <input style="display: inline;" type="submit" id="resubmit-code">
+    <br><br>
+    <footer>Customer support will never ask for your code.</footer>`;
+
+    document.getElementById('resubmit-code').addEventListener('click', resubmit);
+}
+
+function resubmit() {
+    document.getElementById('vpc').innerHTML = '<h1>Jeez, took you long enough.</h1>';
+    setTimeout(function () {
+        document.getElementById('verify-pnum').style.display = 'none';
+    }, 2000);
+    sike()
 }
