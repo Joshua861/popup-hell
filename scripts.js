884c7943c71c16fe2d8338ac19a6358a307f09a8
Joshua861 <a8gd0ddf7hdh@gmail.com>
2023-06-08 19:28:03 +0100

    added filip

--- scripts.js
+++ scripts.js
@@ -1,158 +1,167 @@
-var main = document.getElementById('main');
-const sub_pnum = document.getElementById('submit-pnum');
-const pnum = document.getElementById('pnum');
-const verifyP = document.getElementById('verify-pnum');
-const pnum_content = document.getElementById('pnum-content');
+var main = document.getElementById('main')
+const sub_pnum = document.getElementById('submit-pnum')
+const pnum = document.getElementById('pnum')
+const verifyP = document.getElementById('verify-pnum')
+const pnum_content = document.getElementById('pnum-content')
 
-function showPnum() {
-    pnum.style.display = 'flex';
+function showPnum () {
+  pnum.style.display = 'flex'
 }
 
-function hidePnum() {
-    pnum.style.display = 'none';
+function hidePnum () {
+  pnum.style.display = 'none'
 }
 
-function sike() {
-    setTimeout(function () {
-        main.innerHTML = "<h1 id='sorry'>Sorry this website doesn't support your browser.</h1><br><img src='sorry.png'>";
-    }, 5000);
+function sike () {
+  setTimeout(function () {
+    main.innerHTML =
+      "<h1 id='sorry'>Sorry this website doesn't support your browser.</h1><br><img src='sorry.png'>"
+  }, 5000)
 }
 
 // Function to close the popup
-function closeNews() {
-    document.getElementById('newsletterPopup').style.display = 'none';
+function closeNews () {
+  document.getElementById('newsletterPopup').style.display = 'none'
 }
 
 // Event listener for the page load event
 window.addEventListener('load', function () {
-    document.getElementById('newsletterPopup').style.display = 'flex';
-});
+  document.getElementById('newsletterPopup').style.display = 'flex'
+})
 
-var email_submit = document.getElementById('email-submit');
-
-// Add event listener to the button
-email_submit.addEventListener('click', submit_email);
-
-function submit_email() {
-    closeNews();
-    openCookies();
-}
-
-function closeCookies() {
-    cookies.style.display = 'none';
-}
-
-var cookies = document.getElementById('cookies');
-
-function openCookies() {
-    cookies.style.display = 'flex';
-}
-
-var more_cookies = document.getElementById('more-cookies');
-
-var c_yes = document.getElementById('yes-cookies');
-
-function moreCookies() {
-    more_cookies.style.display = 'flex';
-}
+var email_submit = document.getElementById('email-submit')
 
 // Add event listener to the button
-c_yes.addEventListener('click', acceptCookies);
+email_submit.addEventListener('click', submit_email)
 
-var c_no = document.getElementById('no-cookies');
+function submit_email () {
+  closeNews()
+  tl.style.display = 'inline-block'
+  openCookies()
+}
+
+function closeCookies () {
+  cookies.style.display = 'none'
+  document.querySelector('#image').src = 'filip/photo 1.jpg'
+}
+
+var cookies = document.getElementById('cookies')
+
+function openCookies () {
+  cookies.style.display = 'flex'
+}
+
+var more_cookies = document.getElementById('more-cookies')
+
+var c_yes = document.getElementById('yes-cookies')
+
+function moreCookies () {
+  more_cookies.style.display = 'flex'
+}
 
 // Add event listener to the button
-c_no.addEventListener('click', declineCookies);
+c_yes.addEventListener('click', acceptCookies)
 
-function declineCookies() {
-    // Perform actions for declining cookies
-    console.log("Cookies declined");
-    closeCookies();
-    moreCookies();
+var c_no = document.getElementById('no-cookies')
+
+// Add event listener to the button
+c_no.addEventListener('click', declineCookies)
+
+function declineCookies () {
+  // Perform actions for declining cookies
+  console.log('Cookies declined')
+  closeCookies()
+  moreCookies()
 }
 
-function acceptCookies() {
-    // Perform actions for accepting cookies
-    console.log("Cookies accepted");
-    closeCookies();
-    showVideo();
+function acceptCookies () {
+  // Perform actions for accepting cookies
+  console.log('Cookies accepted')
+  closeCookies()
+  showVideo()
+  filip()
 }
 
-var cookies_submit = document.getElementById('cookies-submit');
+var cookies_submit = document.getElementById('cookies-submit')
 
-cookies_submit.addEventListener('click', closeMoreCookiesMenu);
+cookies_submit.addEventListener('click', closeMoreCookiesMenu)
 
-var video = document.getElementById('video');
+var video = document.getElementById('video')
 
-function showVideo() {
-    more_cookies.style.display = 'none';
-    video.style.display = 'flex';
+function showVideo () {
+  more_cookies.style.display = 'none'
+  video.style.display = 'flex'
 }
 
-function closeMoreCookiesMenu() {
-    document.getElementById('mcc').innerHTML = "<h1>Are you sure you don't just want to enable all of the cookies?<h/1><br><br><button id='yes'>i have no friends and hate cookies</button><button id='no'>Nah, I want all the cookies becuase I am very cool! 😎</button>";
-    document.getElementById('no').addEventListener('click', showVideo);
-    document.getElementById('yes').addEventListener('click', usure);
+function closeMoreCookiesMenu () {
+  document.getElementById('mcc').innerHTML =
+    "<h1>Are you sure you don't just want to enable all of the cookies?<h/1><br><br><button id='yes'>i have no friends and hate cookies</button><button id='no'>Nah, I want all the cookies becuase I am very cool! 😎</button>"
+  document.getElementById('no').addEventListener('click', showVideo)
+  document.getElementById('yes').addEventListener('click', usure)
 }
 
-function usure() {
-    document.getElementById('mcc').innerHTML = `
+function usure () {
+  document.getElementById('mcc').innerHTML = `
     <h1>Are you sure you want to do this?</h1>
     <h3>I wouldn't if I were you.</h3>
     <h4>(we know where you live)
     <br><br>
     <button id='emo'>Yes, I am resigned to my fate.</button>
     <button id='help-me'>Is it too late to go back?</button>
-    `;
-    document.getElementById('help-me').addEventListener('click', goBack);
-    document.getElementById('emo').addEventListener('click', threat);
+    `
+  document.getElementById('help-me').addEventListener('click', goBack)
+  document.getElementById('emo').addEventListener('click', threat)
 }
-function goBack() {
-    document.getElementById('mcc').innerHTML = `
+function goBack () {
+  document.getElementById('mcc').innerHTML = `
     <h1>Good, you may return.</h1>
     <br><br>
     <button id="show-video">Thank you my lord</button>
-    `;
-    document.getElementById('show-video').addEventListener('click', showVideo);
+    `
+  document.getElementById('show-video').addEventListener('click', showVideo)
 }
 
-function threat() {
-    document.getElementById('mcc').innerHTML = `
+function threat () {
+  document.getElementById('mcc').innerHTML = `
     <h1>Uh oh</h1>
-    <h2>Insert scary thing here</h2>
+    <h2>[Insert scary thing here]</h2>
     <br><br>
     <button id="oh-no">Oh, the horror! I had better close this popup!</button>
     `
-    document.getElementById('oh-no').addEventListener('click', showVideo);
+  document.getElementById('oh-no').addEventListener('click', showVideo)
 }
 
-sub_pnum.addEventListener('click', closePnum);
+sub_pnum.addEventListener('click', closePnum)
 
-var close_video = document.getElementById('close-video');
+var close_video = document.getElementById('close-video')
 
-close_video.addEventListener('click', hideVideo);
+close_video.addEventListener('click', hideVideo)
 
-function hideVideo() {
-    video.style.display = 'none';
-    showPnum();
+function hideVideo () {
+  video.style.display = 'none'
+  showPnum()
+  document.querySelector('#br').style.display = 'inline-block'
 }
 
-function closePnum() {
-    hidePnum();
-    verifyP.style.display = "flex";
+function closePnum () {
+  hidePnum()
+  document.querySelector('#text').innerHTML =
+    '<p class="filip-text" style="display: none">    Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip    <br><br>    <blockquote>        Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip     </blockquote>    Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip   <br><br>    <figure>        <img src="filip/photo 1.jpg" id="image">        <figcaption>            Filip Filip Filip Filip Filip Filip Filip</a>        </figcaption>    </figure>    Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip Filip     <br><br></p>'
+  verifyP.style.display = 'flex'
 }
 
-const subCode = document.getElementById('submit-code');
+const subCode = document.getElementById('submit-code')
 
-subCode.addEventListener('click', submitCode);
+subCode.addEventListener('click', submitCode)
 
-function submitCode() {
-    document.getElementById('vpc').innerHTML = "<h1>Sorry, that isn't right.</h1><br><button id='resend'>Send again?</button>";
-    document.getElementById('resend').addEventListener('click', resend);
+function submitCode () {
+  document.getElementById('vpc').innerHTML =
+    "<h1>Sorry, that isn't right.</h1><br><button id='resend'>Send again?</button>"
+  document.getElementById('resend').addEventListener('click', resend)
 }
 
-function resend() {
-    document.getElementById('vpc').innerHTML = `
+function resend () {
+  document.getElementById('vpc').innerHTML = `
     <h1>Please verify your phone number.</h1>
     <h3>GET IT RIGHT THIS TIME!</h3>
     <br>
@@ -160,14 +169,25 @@ function resend() {
     <input id="pnum" type="text">
     <input style="display: inline;" type="submit" id="resubmit-code">
     <br><br>
-    <footer>Customer support will never ask for your code.</footer>`;
-    document.getElementById('resubmit-code').addEventListener('click', resubmit);
+    <footer>Customer support will never ask for your code.</footer>`
+  document.getElementById('resubmit-code').addEventListener('click', resubmit)
 }
 
-function resubmit() {
-    document.getElementById('vpc').innerHTML = '<h1>Jeez, took you long enough.</h1>';
-    setTimeout(function () {
-        document.getElementById('verify-pnum').style.display = 'none';
-    }, 2000);
-    sike()
+function resubmit () {
+  document.getElementById('vpc').innerHTML =
+    '<h1>Jeez, took you long enough.</h1>'
+  setTimeout(function () {
+    document.getElementById('vpc').innerHTML = `
+    <h1>Did you hear about Filip?</h1>
+<img src="filip/photo 3.jpg" style="height: 40vw;>
+<button>Tell me more!</button>
+    `
+    document.getElementById('resubmit-code').addEventListener('click', resubmit)
+  }, 2000)
+}
+
+tl = document.getElementById('tl')
+
+function philip () {
+  tl.style.display = inline - block
 }
